6eb412ec04d6ac004f52bc8fe57db46b60b8418b
chung <lily7891@naver.com>
2024-11-14 00:16:38 +0900

    작업

--- components/desktop/guild/Main.jsx
+++ components/desktop/guild/Main.jsx
@@ -1,8 +1,16 @@
 import React, { memo } from 'react';
+import style from 'css/desktop.module.css';
 
 const Main = () => {
     return (
-        <h1>1</h1>
+        <>
+            <div className={style.profile_box}>
+                <div className={style.num_lv}>
+                    <img src="/img/level/type1/1.svg" alt="레벨 아이콘" />
+                </div>
+                <span className={style.nick}>회원닉네임</span>
+            </div>
+        </>
     );
 }
 
